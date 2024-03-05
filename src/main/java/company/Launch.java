ca060b18aece9f9d296471f94950d5aeb54b88f9
zitchauhan <zitchauhan@gmail.com>
2024-03-06 02:14:58 +0530

    a

--- src/main/java/company/Launch.java
+++ src/main/java/company/Launch.java
@@ -2,34 +2,39 @@ package company;
 
 import java.time.Duration;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 //@Listeners(com.example.MyCustomListener.class)
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Launch {
 
+	   private static final Logger logger = LogManager.getLogger(Launch.class);
+	
     private WebDriver driver;
 
     @BeforeClass
     public void setUp() {
         driver = new ChromeDriver();
+        logger.info("Driver laucnhged ");
         driver.manage().window().maximize();
+        logger.info("Driver maximizd ");
     }
 
     @Test
     public void launchTest() {
         driver.navigate().to("https://www.google.com");
        // driver.findElement(By.xpath("ji"));
+        logger.info("open chrome  ");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         String title = driver.getTitle();
         System.out.println(title);
+        logger.info(title);
     }
     
     
