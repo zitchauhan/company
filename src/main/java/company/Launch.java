a6ce9951b8a9744ad2475909bc65e8a10c393901
zitchauhan <zitchauhan@gmail.com>
2024-03-11 03:19:28 +0530

    changes

--- src/main/java/company/Launch.java
+++ src/main/java/company/Launch.java
@@ -4,11 +4,16 @@ import java.time.Duration;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -24,48 +29,84 @@ public class Launch {
     private static ExtentSparkReporter extentSparkReporter;
     private static ExtentReports extentReports;
     private static ExtentTest extentTest;
+    private static WebDriverWait wait ;
 
     @BeforeClass
     public void setUp() {
-    	
-    	if (driver == null) {
-        extentSparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/test-output/testReport.html");
-        extentReports = new ExtentReports();
-        extentReports.attachReporter(extentSparkReporter);
-        
-        extentSparkReporter.config().setDocumentTitle("Simple Automation Report");
-        extentSparkReporter.config().setReportName("TestReport");
-        extentSparkReporter.config().setTheme(Theme.STANDARD);
-        extentSparkReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a (zzz)");
-        extentSparkReporter.config().setEncoding("UTF-8");
+        if (driver == null) {
+            extentSparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/test-output/testReport.html");
+            extentReports = new ExtentReports();
+            extentReports.attachReporter(extentSparkReporter);
 
-    //    System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        logger.info("Driver launched");
-        driver.manage().window().maximize();
-        logger.info("Driver maximized");
-    }
-    }
-    @Test
-    public void launchTest() {
-        try {
-        extentTest = extentReports.createTest("Open Chrome and navigate to Google", "Get the title");
-        driver.get("https://www.google.com");
-        logger.info("Opened Chrome and navigated to Google");
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        String title = driver.getTitle();
-        logger.info("Title of the page: " + title);
- 
+            extentSparkReporter.config().setDocumentTitle("Simple Automation Report");
+            extentSparkReporter.config().setReportName("TestReport");
+            extentSparkReporter.config().setTheme(Theme.STANDARD);
+            extentSparkReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a (zzz)");
+            extentSparkReporter.config().setEncoding("UTF-8");
+
+            WebDriverManager.chromedriver().setup();
             
-            extentTest.log(Status.PASS, "Test Passed");
-        } catch (Exception e) {
-            extentTest.log(Status.FAIL, e);
-            throw e;
+            driver = new ChromeDriver();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+            
+            logger.info("Driver launched");
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().maximize();
+            logger.info("Driver maximized");
+            
+            wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         }
     }
 
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void launchTest() {
+        try {
+            extentTest = extentReports.createTest("Open Chrome and navigate to Google", "Get the title");
+            driver.get("https://www.google.com");
+            logger.info("Opened Chrome and navigated to Google");
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+            String title = driver.getTitle();
+            System.out.println(title);
+            logger.info("Title of the page: " + title);
+
+            SoftAssert softAssert = new SoftAssert();
+            softAssert.assertEquals(title, "Google");
+            softAssert.assertAll();
+
+            extentTest.log(Status.PASS, "Test Passed");
+        } catch (AssertionError e) {
+            extentTest.log(Status.FAIL, "Test Failed: " + e.getMessage());
+            throw e;
+        }
+    }
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void TestGmailClick() {
+        try {
+            extentTest = extentReports.createTest("Open Chrome and navigate to Google", "Get the title");
+            driver.get("https://www.google.com");
+            logger.info("Opened Chrome and navigated to Google");
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+            String title = driver.getTitle();
+            System.out.println(title);
+            logger.info("Title of the page: " + title);
+
+            SoftAssert softAssert = new SoftAssert();
+            softAssert.assertEquals(title, "Google");
+            softAssert.assertAll();
+            WebElement ele  = driver.findElement(By.xpath("//a[contains(text() , 'Gmail')]"));
+            
+            wait.until(ExpectedConditions.visibilityOf(ele));
+            ele.click();
+            
+            logger.info(" click on gmail");
+            extentTest.log(Status.PASS, "Test Passed");
+        } catch (AssertionError e) {
+            extentTest.log(Status.FAIL, "Test Failed: " + e.getMessage());
+            throw e;
+        }
+    }
     @AfterClass
     public void tearDown() {
         try {
--- src/main/java/company/RetryAnalyzer.java
+++ src/main/java/company/RetryAnalyzer.java
@@ -1,11 +1,12 @@
 package company;
+
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
-
 public class RetryAnalyzer implements IRetryAnalyzer {
-    private static final int MAX_RETRY_COUNT = 3;
+
     private int retryCount = 0;
+    private static final int MAX_RETRY_COUNT = 3; // Maximum number of retry attempts
 
     @Override
     public boolean retry(ITestResult result) {
