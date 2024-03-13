e24ef6e721f96c94f7ff8781dbba8a0fe4491be3
zitchauhan <zitchauhan@gmail.com>
2024-03-13 14:10:33 +0530

    new changes

--- src/main/java/company/Utility.java
+++ src/main/java/company/Utility.java
@@ -1,5 +1,25 @@
-package company;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 
 public class Utility {
 
+    public static String captureScreenshot(WebDriver driver, ITestResult result) 
+    
+   {
+        String screenshotPath = "";
+        try
+        {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            screenshotPath = "./test-output/screenshots/" + result.getName() + "_" + System.currentTimeMillis() + ".png";
+            FileUtils.copyFile(screenshot, new File(screenshotPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return screenshotPath;
+    }
 }
