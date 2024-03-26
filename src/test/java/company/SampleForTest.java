683bcadd271f51ca66585e1b94c182dc1573dfa7
zitchauhan <zitchauhan@gmail.com>
2024-03-26 14:01:29 +0530

    newchanges

---
+++ src/test/java/company/SampleForTest.java
@@ -0,0 +1,68 @@
+package company;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleForTest {
+
+	@Test
+	public void demotest1()
+
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+	@Test
+	public void demotest2()
+
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+	@Test
+	public void demotest3()
+
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+	@Test
+	public void demotest4()
+
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+	@Test
+	public void demotes5()
+
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+	@Test
+	public void demotest6()
+
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+}
